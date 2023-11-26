65b4487b084198736a6436dec42e5fc44d3d6802
Rory Martin <rorymartin3d@gmail.com>
2023-11-26 14:29:22 +0000

    Basic Calculation functions added
    calculations for 2 seperate numbers created along side a 'operate'
    function that uses a switch statement to identify which operator
    determines each function

---
+++ main.js
@@ -0,0 +1,39 @@
+// quokka: Cmd/Ctrl + K, Q
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            'Invalid Number';
+            break;
+    }
+}
+
+console.log(operate(2, 5, '*'));
